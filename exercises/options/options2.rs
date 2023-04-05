bdf659fd8bd34ea2f49e898a95b3441cbd1df12f
jhwzju <129476037+jhwzju@users.noreply.github.com>
2023-04-05 15:16:54 +0000

    51

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -9,10 +9,12 @@ mod tests {
     fn simple_option() {
         let target = "rustlings";
         let optional_target = Some(target);
-
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        // word = optional_target {
+        //     assert_eq!(word, target);
+        // }
+        if let Some(target) = optional_target {
+            assert_eq!(optional_target.unwrap(), target);
         }
     }
 
@@ -23,12 +25,14 @@ mod tests {
         for i in 0..(range + 1) {
             optional_integers.push(Some(i));
         }
-
+        
+        let mut option = optional_integers.pop();
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`'s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, range);
-            range -= 1;
+        while let Some(i) = option {
+            if let Some(i) = option {
+                option = optional_integers.pop()
+            } 
         }
     }
 }
