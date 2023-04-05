53d0e295c5d6cc50455b09729780d1594195c068
jhwzju <129476037+jhwzju@users.noreply.github.com>
2023-04-05 14:57:48 +0000

    49

--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -17,8 +17,7 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|num| { num*2
-    }).collect() 
+    v.iter().map(|num| { num*2 }).collect() 
 }
 
 #[cfg(test)]
