d421d8ccbdbb0348d4a6083b4731546e9ed99196
jhwzju <129476037+jhwzju@users.noreply.github.com>
2023-04-05 11:30:05 +0000

    41

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,21 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
+    input.trim().to_string()
     // TODO: Remove whitespace from both ends of a string!
-    ???
 }
 
 fn compose_me(input: &str) -> String {
+    (input.to_owned() + " world!").to_string()
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons").to_string()
 }
 
 #[cfg(test)]
